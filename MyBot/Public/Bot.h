137792d33375d8809a587967c86f7c861b2ed57d
Pierrhum <34352456+Pierrhum@users.noreply.github.com>
2023-04-19 14:47:42 +0200

    Rider project creation from c++ source files

---
+++ MyBot/Public/Bot.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "State.h"
+
+/*
+    This struct represents your bot in the game of Ants
+*/
+struct Bot
+{
+    State state;
+
+    Bot();
+
+    void playGame();    //plays a single game of Ants
+
+    void makeMoves();   //makes moves for a single turn
+    void endTurn();     //indicates to the engine that it has made its moves
+};
---
+++ MyBot/Public/Location.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+/*
+    struct for representing locations in the grid.
+*/
+struct Location
+{
+    int row, col;
+
+    Location()
+    {
+        row = col = 0;
+    };
+
+    Location(int r, int c)
+    {
+        row = r;
+        col = c;
+    };
+};
