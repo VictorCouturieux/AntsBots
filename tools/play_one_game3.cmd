21b0b39cdf0709754f51b687f15db9748417d221
VictorCouturieux <victor.couturieux@laposte.net>
2023-04-29 13:14:45 +0200

    play one game 3 mybot*4

---
+++ tools/play_one_game3.cmd
@@ -0,0 +1,1 @@
+python "%~dp0playgame.py" --engine_seed 42 --player_seed 42 --end_wait=0.25 --verbose --log_dir game_logs --turns 500 --map_file "%~dp0maps\maze\maze_04p_01.map" %* "..\MyBot\MyBot.exe" "..\MyBot\MyBot.exe" "..\MyBot\MyBot.exe" "..\MyBot\MyBot.exe"
